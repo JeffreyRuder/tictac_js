2fe4ecd371ea582c98ec0f5ffb681208fd3a763d
Chris Siems and Jeffrey Ruder <csiems@gmail.com>
2016-02-10 14:12:06 -0800

    Improve styling

--- js/ui.js
+++ js/ui.js
@@ -1,23 +1,34 @@
 $(function() {
   var game = new Game;
+  var gameOn = true;
 
   $(".board-cell").click(function(event) {
-    console.log(game);
+    if (!gameOn) {
+      return;
+    }
     var cellId = $(this).attr('id').split(",");
     var cellX = parseInt(cellId[0]);
     var cellY = parseInt(cellId[1]);
-    console.log(game.board);
-    console.log(cellId);
-    game.board.getSpace(cellX, cellY).markedBy(game.playerTurn);
-    if(game.playerTurn === game.playerOne) {
-      $(this).css("background-color", "blue");
-    } else {
-      $(this).css("background-color", "red");
-    }
-    game.nextTurn();
-    if (game.winner) {
-      alert(game.winner.mark + " wins!");
+    thisSpace = game.board.getSpace(cellX, cellY);
+    if (!thisSpace.isMarked) {
+      thisSpace.markedBy(game.playerTurn);
+      if(game.playerTurn === game.playerOne) {
+        $(this).append("<span class='letter x'>X</span>");
+      } else {
+        $(this).append("<span class='letter o'>O</span>");
+      }
+      game.nextTurn();
+      if (game.winner && game.winner !== "Draw") {
+        $(".turn-info").empty().append("<h2>Player " + game.winner.mark + " wins!</h2>");
+        gameOn = false;
+        $(".reset-button").show();
+      } else if (game.winner) {
+        $(".turn-info").empty().append("<h2>It's a draw!</h2>");
+        gameOn = false;
+        $(".reset-button").show();
+      } else {
+        $(".turn-info").empty().append("<h2>Player " + game.playerTurn.mark + "'s turn!</h2>");
+      }
     }
   });
-
 })
